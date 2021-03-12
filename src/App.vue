00bd630d5ea18d7e927d384e2ff6ced248978bfe
Qusavin Fillov <qusavin@list.ru>
2021-03-12 12:47:19 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+	<component :is="layout + '-layout'" />
+</template>
+
+<script>
+import { computed, } from 'vue';
+import { useRoute, } from 'vue-router';
+
+import AuthLayout from './layouts/AuthLayout.vue';
+import MainLayout from './layouts/MainLayout.vue';
+
+
+export default {
+	setup() {
+		const route = useRoute();
+
+		const layout = computed(() => route.meta.layout);
+
+		return {
+			layout,
+		};
+	},
+	components: {
+		AuthLayout,
+		MainLayout,
+	},
+};
+</script>
+
+
+<style lang="scss">
+
+</style>
